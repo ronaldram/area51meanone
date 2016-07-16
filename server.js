a8c7e132df01e3b41fb9c0fc9c1779974066dae0
Ronald <ronald.ameri@gmail.com>
2016-07-15 22:06:08 -0500

    clase 5

--- server.js
+++ server.js
@@ -1,130 +1,75 @@
 var express = require('express');
-var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/pokemon');
-mongoose.connect('mongodb://admin:admin@ds019472.mlab.com:19472/pokemon_ram')
-console.log(mongoose);
+
 var app = express();
 var path = require('path');
-var adminRouter = express.Router();
-var loginRouter = express.Router();
-var errorRouter = express.Router();
+//var adminRouter =   express.Router();
+//var loginRouter = express.Router();
+//var errorRouter = express.Router();
 
-app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname)+'/index.html');
-});
+var bodyParser=require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var User = require('./dbscript/User');
+console.log(mongoose);
 
+var port = process.env.PORT || 5000;
 
-//middleware
-adminRouter.use(function(req, res, next){
- console.log(req.method, req.url);
- next();
-});
+//APP CONFIGURATION
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-adminRouter.param('name', function(req,res,next,name){
-  console.log('req name',req.params.name);
-  console.log('name',name);
-  req.name = "MR. robot was here!";
+//CORS
+app.use(function(req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-Width, content-type, Authorization');
   next();
 });
 
-loginRouter.use(function(req, res, next){
- console.log('loginRouter.use', req.method, req.url);
- console.log('loginRouter.login', req.userlogin, req.password);
- next();
+app.use(morgan('dev'));
+//db connection
+mongoose.connect('mongodb://localhost/pokemon');
+//mongoose.connect('mongodb://admin:admin@ds019472.mlab.com:19472/pokemon_ram')
+
+//API ROUTERS
+
+//main basic route
+app.get('/', function(req, res){
+  res.send('welcome to the real world')
 });
 
-loginRouter.param('userlogin', function(req, res,next,userlogin){
-	console.log('-------userlogin----------')
-	console.log('req userlogin', req.params.userlogin);
-	console.log('userlogin', userlogin);
+//express router instance
+var apiRouter = express.Router();
 
-	if(userlogin=='pepito'){
-		req.userlogin=userlogin;
-	}else{
-		console.log('user invalid!');
-	}
-	console.log('--------userlogin---------')
-	next();
-
+//Accessed at GET http://localhost:5000/api
+apiRouter.get('/', function(res,res){
+  res.json({message:'Stop to try hit me and hit me!'})
 });
 
-loginRouter.param('password', function(req, res, next, password){
-	console.log('-------password----------')
-	if(password=="lima123"){
-		req.password=password;
-		next();
-	}else{
-		console.log('password invalid!');
-		res.redirect('/error');
-	}
-	console.log('-------userlogin----------')
-
-});
-
-//router - users
-adminRouter.get('/', function(req, res){
-	res.send('Estoy en la pagina princial');
-})
-
-adminRouter.get('/users', function(req, res){
-	console.log('users view');
-	res.send('Aqui se mostraran los usuarios');
-})
-
-adminRouter.get('/users/:name', function(req, res){
-	console.log('users name');
-	res.send('Hola '+req.name);
-})
-
-
-adminRouter.get('/posts', function(req, res){
-	res.send('Aqui se mostraran los articulos');
-})
-
-//router login
-
-loginRouter.get('/', function(req,res){
-	res.send('Estoy en el login');
-});
-
-
-loginRouter.get('/authenticar/:userlogin/:password', function(req,res){
-	console.log('userlogin view', req.userlogin);
-	console.log('password view', req.password);
-
-	if(req.userlogin && req.password)
-		res.send('Bienvenido Don '+req.userlogin);
-	else
-		res.send('Usuario Invalido, intentelo nuevamente!');
-});
-
-errorRouter.get('/', function(req, res){
-	res.send('User invalido');
-});
-
-app.use('/userlogins', loginRouter);
-app.use('/admin', adminRouter);
-app.use('/error', errorRouter);
-
-app.route('/account')
-.get(function(req,res){
-	console.log('account get');
-	res.send('acount get');
-})
+//Routers /users
+apiRouter.route('/users')
+//Create a user through post
+//URL:  http://localhost:5000/api/users
 .post(function(req,res){
-	console.log('account post');
-	res.send('acount post');
-})
-.put(function(req,res){
-	console.log('account put');
-	res.send('acount put');
-})
-.delete(function(req,res){
-	console.log('account delete');
-	res.send('acount delete');
-});
+  var user = new User();
+  user.name = req.body.name;
+  user.username = req.body.username;
+  user.password = req.body.password;
+  user.save(function(err){
+    //verify duplicate entry on username
+    console.log(err);
+    if(err.code==11000){//mongo retorn this code
+      return res.json({sucess:false, message:'El nombre es duplicado'});
+    }
 
+    res.json({success:true, message:''});
+  });
+})
+.get(function(req,res){
 
-app.set('port', (process.env.PORT || 5000))
-app.listen(app.get('port'));
+})
+
+//Register our router
+app.use('/api', apiRouter);
+app.listen(port);
 console.log('Here we are go');
